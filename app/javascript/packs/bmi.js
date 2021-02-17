d848b29a267f388c23a10f09768b671d6397dae7
hassi-INR <hassi.d.cat@gmail.com>
2021-02-17 16:51:26 +0900

    Javascrpt編集

--- app/javascript/packs/bmi.js
+++ app/javascript/packs/bmi.js
@@ -1,26 +1,24 @@
-// 入力要素を取得
-var height = document.getElementById('height-input'); // 身長入力欄
-var weight = document.getElementById('weight-input'); // 体重入力欄
-var button = document.getElementById('button-submit'); // 計測開始ボタン
+var height = document.getElementById('height-input');
+var weight = document.getElementById('weight-input');
+var button = document.getElementById('button-submit');
 
-// 出力要素を取得
-var output = document.getElementById('bmi-output'); // BMI表示場所
+var output = document.getElementById('bmi-output'); 
 
 // 入力値からBMIを計算して指定場所に表示する関数
 var calcBmi = function () {
-  // 1.入力された身長の値 (cm)を取得
+  // 身長の値
   var h_value = height.value;
-  // 2.入力された体重の値 (kg)を取得
+  // 体重の値
   var w_value = weight.value;
-  // 3.身長(cm)をメートルに換算する
+  // 身長(cm)をメートル
   h_value /= 100;
-  // 4.BMIを計算する：体重 ÷ (身長 m × 身長 m)
+  // BMIを計算：体重 ÷ (身長 m × 身長 m)
   var bmi = w_value / (h_value * h_value);
-  // 5.BMIの小数点第一位以下切り捨て
+  // BMIの小数点第一位以下切り捨て
   bmi = Math.floor(bmi * 10) / 10;
-  // 6.BMIを表示
+  // BMIを表示
   output.innerHTML = bmi;
 }
 
-// 計測開始ボタンが押されたときの処理を登録（イベントリスナー）
+
 button.addEventListener('click', calcBmi);
