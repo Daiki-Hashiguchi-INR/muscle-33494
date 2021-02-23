7d0ae79c2db2cb8d0ab0097cce228d7d59c7d607
hassi-INR <hassi.d.cat@gmail.com>
2021-02-23 19:11:24 +0900

    ユーザーモデル　結合コード作成

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  context 'ユーザー登録ができるとき' do
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード（6文字以上）', with: @user.password
+      fill_in 'パスワード再入力', with: @user.password_confirmation
+      fill_in 'ユーザー名', with: @user.nickname
+      fill_in 'プロフィール', with: @user.profile
+      fill_in '目標', with: @user.goal
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      # トップページへ遷移する
+      expect(current_path).to eq(root_path)
+      # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'メールアドレス', with: ''
+      fill_in 'パスワード（6文字以上）', with: ''
+      fill_in 'パスワード再入力', with: ''
+      fill_in 'ユーザー名', with: ''
+      fill_in 'プロフィール', with: ''
+      fill_in '目標', with: ''
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq('/users')
+    end
+  end
+end
